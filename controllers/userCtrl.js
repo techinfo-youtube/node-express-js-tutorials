431201ec67dc3518ea4c5c466aa0a32533b1d4c5
Rasheed khan <rktechnicalplus@gmail.com>
2022-10-05 10:55:09 +0530

    update and delete operations

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,3 +1,5 @@
+const UserModel = require("../models/userModel");
+
 const getUser = (req, res) => {
   res.send({
     success: true,
@@ -13,4 +15,107 @@ const addUser = (req, res) => {
   });
 };
 
-module.exports = { getUser, addUser };
+const createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await UserModel.create({
+      name,
+      email,
+      password,
+    });
+    res.status(201).json({
+      message: "success",
+      user,
+    });
+  } catch (error) {
+    console.log(`errro in create user ctrl = ${error}`);
+    res.status(400).json({
+      message: false,
+      error,
+    });
+  }
+};
+
+//get all users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json({
+      success: true,
+      totalUsers: users.length,
+      users,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      msg: "get all user error",
+
+      error: error.message,
+    });
+  }
+};
+
+//GET SINGLE USER
+const getSingleUser = async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.id);
+    if (user) {
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } else {
+      res.status(400).send("User Not Found");
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Single User Error",
+      error: error.message,
+    });
+  }
+};
+
+//Update Users
+const updateUser = async (req, res) => {
+  const user = await UserModel.findById(req.params.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(201).json({
+      success: true,
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("User Not FOund");
+  }
+};
+//Delete User
+const deleteUser = async (req, res) => {
+  const user = await UserModel.findById(req.params.id);
+  if (user) {
+    await user.remove();
+    res.status(200).json({
+      success: true,
+      message: "User Deleted",
+    });
+  } else {
+    res.status(400);
+    throw new Error("User Not Found");
+  }
+};
+module.exports = {
+  createUser,
+  getAllUsers,
+  getSingleUser,
+  deleteUser,
+  updateUser,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,30 @@
 const express = require("express");
-const { getUser, addUser } = require("../controllers/userCtrl");
+const {
+  createUser,
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userCtrl");
 
 //routes router object
 const router = express.Router();
 
 //routes
 
-//GET USER ||  METHOD = GET
-router.get("/", getUser);
+//CREATE NEW USER
+router.post("/create-user", createUser);
 
-//POST USER || METHOD POST
-router.post("/add-user", addUser);
+//GET ALL USERS
+router.get("/get-users", getAllUsers);
 
+//GET SINGLE USER
+router.get("/get-users/:id", getSingleUser);
+
+//UPDATE USER
+router.put("/update-user/:id", updateUser);
+
+//DELETE USER
+router.delete("/delete-user/:id", deleteUser);
 //export routes
 module.exports = router;
